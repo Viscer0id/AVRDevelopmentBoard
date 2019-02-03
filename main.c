843789d4e44ed3cde2779b693fdf26fb52c3e222
Jeremy <jeremy.dennis.au@gmail.com>
2019-02-03 19:22:29 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+/* Name: main.c
+ * Author: Jeremy Dennis
+ * Copyright:
+ * License:
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/sleep.h>
+
+int main(void) {
+   return 0;
+}
